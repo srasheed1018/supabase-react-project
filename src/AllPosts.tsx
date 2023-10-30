eae178c7cbb8523813fab9a4013ba95f99559521
Siddeeq Rasheed <srasheed1018@gmail.com>
2023-10-30 18:29:48 -0400

    added page selector

--- src/AllPosts.tsx
+++ src/AllPosts.tsx
@@ -17,30 +17,40 @@ export default function AllPosts() {
   const [myVotes, setMyVotes] = useState<
     Record<string, "up" | "down" | undefined>
   >({});
+  const [totalPages, setTotalPages] = useState(0);
   useEffect(() => {
     const queryPageNumber = pageNumber ? +pageNumber : 1;
-    supaClient
-      .rpc("get_posts", { page_number: queryPageNumber })
-      .select("*")
-      .then(({ data }) => {
-        setPosts(data as GetPostsResponse[]);
-        if (session?.user) {
-          supaClient
-            .from("post_votes")
-            .select("*")
-            .eq("user_id", session.user.id)
-            .then(({ data: votesData }) => {
-              if (!votesData) {
-                return;
-              }
-              const votes = votesData.reduce((acc, vote) => {
-                acc[vote.post_id] = vote.vote_type as any;
-                return acc;
-              }, {} as Record<string, "up" | "down" | undefined>);
-              setMyVotes(votes);
-            });
-        }
-      });
+    Promise.all([
+      supaClient
+        .rpc("get_posts", { page_number: queryPageNumber })
+        .select("*")
+        .then(({ data }) => {
+          setPosts(data as GetPostsResponse[]);
+          if (session?.user) {
+            supaClient
+              .from("post_votes")
+              .select("*")
+              .eq("user_id", session.user.id)
+              .then(({ data: votesData }) => {
+                if (!votesData) {
+                  return;
+                }
+                const votes = votesData.reduce((acc, vote) => {
+                  acc[vote.post_id] = vote.vote_type;
+                  return acc;
+                }, {} as Record<string, "up" | "down" | undefined>);
+                setMyVotes(votes);
+              });
+          }
+        }),
+      supaClient
+        .from("posts")
+        .select("*", { count: "exact", head: true })
+        .filter("path", "eq", "root")
+        .then(({ count }) => {
+          count == null ? 0 : setTotalPages(Math.ceil(count / 10));
+        }),
+    ]);
   }, [session, bumper, pageNumber]);
 
   return (
@@ -52,6 +62,12 @@ export default function AllPosts() {
           }}
         />
       )}
+
+      <Pagination
+        totalPages={totalPages}
+        currentPage={pageNumber ? +pageNumber : 0}
+      />
+
       <div className="posts-container">
         {posts?.map((post, i) => (
           <Post
@@ -65,6 +81,71 @@ export default function AllPosts() {
         ))}
       </div>
     </>
+  );
+}
+
+const selectedStyles = "border-2 border-white rounded p-2 bg-gray-700 text-green-400";
+const notSelectedStyles = "rounded p-2 bg-gray-700";
+
+function Pagination({
+  totalPages,
+  currentPage,
+}: {
+  currentPage: number;
+  totalPages: number;
+}) {
+  const middleButtons = [currentPage];
+  for (let i = currentPage - 1; i > 0 && i > currentPage - 5; i--) {
+    middleButtons.unshift(i);
+  }
+  for (let i = currentPage + 1; i <= totalPages && i <= currentPage + 4; i++) {
+    middleButtons.push(i);
+  }
+  return (
+    <div className="flex justify-center gap-4 place-items-end">
+      {currentPage > 5 ? (
+        <Link
+          data-e2e={`page-1`}
+          className={notSelectedStyles}
+          to={`/1`}
+          key={1}
+        >
+          1
+        </Link>
+      ) : (
+        <></>
+      )}
+      {currentPage > 6 ? <span data-e2e="starting-elipsis"> ... </span> : <></>}
+      {middleButtons.map((pageNumber) => (
+        <Link
+          key={pageNumber}
+          data-e2e={`page-${pageNumber}`}
+          className={
+            currentPage === pageNumber ? selectedStyles : notSelectedStyles
+          }
+          to={`/${pageNumber}`}
+        >
+          {pageNumber}
+        </Link>
+      ))}
+      {totalPages - currentPage > 5 ? (
+        <span data-e2e="ending-elipsis"> ... </span>
+      ) : (
+        <></>
+      )}
+      {totalPages - currentPage > 4 ? (
+        <Link
+          data-e2e={`page-${totalPages}`}
+          className={notSelectedStyles}
+          to={`/${totalPages}`}
+          key={totalPages}
+        >
+          {totalPages}
+        </Link>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 }
 
